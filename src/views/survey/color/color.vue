ba55e761c67cb215f92049f32f7e6978b7c40da4
Yunjeong Goo <heidiyun.goo@gmail.com>
2019-12-09 16:32:10 +0900

    color page ..ing

--- src/views/survey/color/color.vue
+++ src/views/survey/color/color.vue
@@ -1,22 +1,24 @@
 <template>
-  <v-container fill-height fluid class="pa-0":style="selectedColor === '' ? {backgroundColor: 'black'} :{backgroundColor: selectedColor}" >
+  <v-container fill-height fluid class="pa-0":style="selectedColor === '' && selectedColors.length===0 ? {backgroundColor: 'black'} :{backgroundColor: selectedColors.length === 1 ?selectedColors[0] : selectedColor}" >
   <h1 class='survey-question' style="color:rgba(255,255,255,1);">Select {{selectedColors.length === 0 ? 'start' : 'end'}} color what you want</h1>  
    
-
+   <div v-if="selectedColors.length === 1" class="second-color-container" :style="{backgroundImage: `linear-gradient( ${selectedColors[0]} , ${selectedColor}  35% 55%, ${selectedColors[0]})`}"></div>
     
-    <v-layout justify-center align-center style="position:absolute; bottom:0; height: 40px; width:100%; background:rgba(0,0,0,1); margin-bottom: 20px;" >
+    <v-layout justify-center align-center style="position:absolute; bottom:50px; height: 40px; width:100%; background:rgba(0,0,0,1); " >
       <div v-for="(color ,i ) in colors"
       class="color-div"
       :key="`color${i}`"
-      :selected="color === selectedColor"
+      :selected="color === selectedColor || selectedColors[0] === color"
       :disabled="selectedColors.length === 2"
       :style="{backgroundColor:color}"
       @click="onClickColor(color)"
       ></div>
+      
     </v-layout>
-    <!-- <div class="d-flex" style="width: 100vw; justify-content : flex-end;">
-    <v-btn @click="onSelect">{{selectedColors.length === 0 ? '선택' : '확인'}}</v-btn>
-    </div> -->
+    
+    <div class="d-flex" style="width: 100vw; justify-content : center; position:absolute; bottom:10px;">
+    <v-btn icon dark  @click="onSelect"><v-icon> mdi-check-bold</v-icon></v-btn>
+    </div>
     
   </v-container>
 </template>
